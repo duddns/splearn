17db1fde733475317e9e816d4cc0d6e4451e618f
duddns <duddns@gmail.com>
2025-07-10 18:46:29 +0900

    도메인 로직 개발

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -1,20 +1,44 @@
 package tobyspring.splearn.domain;
 
+import java.util.Objects;
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 @Getter
 public class Member {
 
+  /*
+   * property 줄 붙여쓰는것이 좋은가? 띄어쓰는것이 좋은가?
+   *
+   */
+
   private String email;
+
   private String nickname;
+
   private String passwordHash;
+
   private MemberStatus status;
 
 
   public Member(String email, String nickname, String passwordHash) {
-    this.email = email;
-    this.nickname = nickname;
-    this.passwordHash = passwordHash;
+    this.email = Objects.requireNonNull(email);
+    this.nickname = Objects.requireNonNull(nickname);
+    this.passwordHash = Objects.requireNonNull(passwordHash);
+
     this.status = MemberStatus.PENDING;
+  }
+
+  public void activate() {
+    // spring framework 의존성이 추가 되지만, utils 함수를 사용하는 정도는 큰 문제가 아니다. apache commons 사용하는 것과 마찬가지
+    Assert.state(status == MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
+
+    this.status = MemberStatus.ACTIVE;
+  }
+
+  public void deactivate() {
+    Assert.state(status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
+
+    this.status = MemberStatus.DEACTIVATED;
   }
 }
--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -14,4 +14,52 @@ class MemberTest {
     Assertions.assertThat(member.getPasswordHash()).isEqualTo("passwordHash1");
     Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
   }
+
+  @Test
+  void test_constructorNullCheck() {
+    Assertions.assertThatThrownBy(() -> new Member(null, "nickname1", "passwordHash1"))
+        .isInstanceOf(NullPointerException.class);
+  }
+
+  @Test
+  void test_activate() {
+    Member member = new Member("test@test.com", "nickname1", "passwordHash1");
+
+    member.activate();
+
+    Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+  }
+
+  @Test
+  void test_activateFail() {
+    Member member = new Member("test@test.com", "nickname1", "passwordHash1");
+
+    member.activate();
+
+    Assertions.assertThatThrownBy(() -> {
+      member.activate();
+    }).isInstanceOf(IllegalStateException.class);
+  }
+
+  @Test
+  void test_deactivate() {
+    Member member = new Member("test@test.com", "nickname1", "passwordHash1");
+
+    member.activate();
+    member.deactivate();
+
+    Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+  }
+
+  @Test
+  void test_deactivateFail() {
+    Member member = new Member("test@test.com", "nickname1", "passwordHash1");
+
+    member.activate();
+    member.deactivate();
+
+    Assertions.assertThatThrownBy(() -> {
+      member.activate();
+    }).isInstanceOf(IllegalStateException.class);
+  }
 }
